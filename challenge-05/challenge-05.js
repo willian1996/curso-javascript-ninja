4b64fbc4e6e79ad57a96a4a795b52d351fb88dbe
Willian Sales <williansalesgabriel@hotmail.com>
2019-02-16 04:48:23 -0200

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,20 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myVar = ['Willian', true, false, 23, 1.76];
+
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function receberArray(param){
+    return param;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+receberArray(myVar)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +23,30 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function myFunction(array1, indice){
+    return array1[indice];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var newVar = ['string', 56, true, false, 1.78];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+myFunction(newVar, 0);
+//"string"
+myFunction(newVar, 1);
+//56
+myFunction(newVar, 2);
+//true
+myFunction(newVar, 3);
+//false
+myFunction(newVar, 4);
+//1.78
+
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,7 +62,36 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+
+function book(nomeLivro){
+    var obj = {
+        harry_potter_pedra_filosofal: {
+            quantidadePaginas: 482,
+            autor: 'J. K. Rowling',
+            editora: 'englishEditor',
+        },
+        game_of_thrones: {
+            quantidadePaginas: 250,
+            autor: 'George R. R. Martin',
+            editora: 'Jones',
+        },
+        the_walking_dead: {
+            quantidadePaginas: 189,
+            autor: 'Jay Bonansinga',
+            editora: 'Kirkman',
+        }
+    }
+    var interBook = nomeLivro;
+    if(nomeLivro != undefined){
+        return obj.interBook;
+    }else{
+        return obj;
+    }
+}
+
+
+
+
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
