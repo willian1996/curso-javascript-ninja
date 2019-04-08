f68ebf926525e22b32f177b78c1143e9d0af2acd
Willian Sales <williansalesgabriel@hotmail.com>
2019-04-08 17:50:59 -0300

    Aula 08/04

---
+++ _Aula/Aula-16-semana16/js/main.js
@@ -0,0 +1,60 @@
+(function(){
+    
+    
+    
+    
+    
+    /*
+    'use strict';
+    function Person(name, lastName, age){
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        
+    }
+    console.log(new Person('Willian', 'Sales', 30));
+    */
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    //use strict não permite utilizar o with
+    'use strict';
+    var obj = {
+        prop1:{
+            prop2:{
+                prop3:{
+                    prop31:'prop31',
+                    prop32:'prop32',
+                    prop33:'prop33'
+
+                }  
+            }
+        }
+    };
+    console.log(obj.prop1.prop2.prop3.prop31);
+    console.log(obj.prop1.prop2.prop3.prop32);
+    console.log(obj.prop1.prop2.prop3.prop33);
+    with(obj.prop1.prop2.prop3){
+        console.log(prop31, prop32, prop33);
+         
+    }
+    */
+    
+    
+
+    /*
+    //use strict não permite declarar variaveis sem o var
+    'use strict';
+     var myName = 'Willian Sales';
+    
+    console.log(myName);
+    
+    */
+})();
+
