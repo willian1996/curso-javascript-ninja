0d9f6045b839e77e6885998d739cabe71f5ed161
Willian Sales <williansalesgabriel@hotmail.com>
2019-04-11 04:44:31 -0300

    Aula 11/04

--- _Aula/Aula-16-semana16/js/main.js
+++ _Aula/Aula-16-semana16/js/main.js
@@ -1,5 +1,42 @@
 (function(){
     
+    var will = 'willian';
+    console.log(will);
+    
+    //passando uma string para maiuscula
+    var newWill = will.toUpperCase();
+    console.log(newWill);
+    
+    
+    //passando uma string para minuscula
+    newWill = newWill.toLowerCase();
+    console.log(newWill);
+    
+    
+    //passando a primeira letra para maiuscula
+    newWill = will.charAt(0).toUpperCase() + will.slice(1);
+    console.log(newWill);
+    
+    
+    /*
+    function myFunction(a, a, b){
+        'use sctrict';
+        return a + b;
+    }
+    console.log(myFunction(1,2,3));
+    */
+    
+    
+    /*
+    'use strict';
+    var myVar = 2;
+    var obj = {
+        prop1: 'prop1',
+        prop1: 'prop2',
+        prop3: 'prop3'
+    };
+    console.log(obj);
+    */
     
     
     
