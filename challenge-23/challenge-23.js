630aa8f0aa0153c7f7ff52f12eeac825f5d3a4e6
Willian Sales <williansalesgabriel@hotmail.com>
2019-06-07 13:07:53 -0300

    Aula 07/06

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -28,16 +28,49 @@
     
     var $visor = doc.querySelector('[data-js="visor"]');
     var $buttonsNumbers = doc.querySelectorAll('[data-js="button-number"]');
+    var $buttonsOperations = doc.querySelectorAll('[data-js="button-operation"]');
+    var $buttonCE = doc.querySelector('[data-js="button-ce"]');
     
     
     Array.prototype.forEach.call($buttonsNumbers, function(button){
         button.addEventListener('click', handleClickNumber, false);
         
     });
+    $buttonCE.addEventListener('click', handleClickCE, false);
     
-    function handleClickNumber(event){
+    Array.prototype.forEach.call($buttonsOperations, function(button){
+        button.addEventListener('click', handleClickNumber, false);
+        
+    });
+    
+
+    
+    function handleClickNumber(){
         $visor.value += this.value;
     }
+    function handleClickOperation(){
+        
+        if(isLastItemAnOperation()){
+            $visor.value = $visor.value.slice(0, -1);
+        }
+            
+        $visor.value += this.value;
+    }
+    
+    function handleClickCE(){
+        $visor.value = 0;
+    }
+    
+    function isLastItemAnOperation(){
+        var operations = ['+', '-', '*', '/'];
+        var lastItem = $visor.value.split('').pop();
+        return operations.some(function(operator){
+            return operator === lastItem;
+        });
+        console.log(lastItem);
+    }
+    
+    
     
     
     
